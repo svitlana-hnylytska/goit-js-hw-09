f1a3efb1ccd8cdaec219373241a66f934d1038f4
Svitlana Hnylytska <alyate173@gmail.com>
2026-09-20 17:09:48 +0300

    update 2-form.js

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,8 +5,38 @@ const formData = {
 
 const form = document.querySelector(".feedback-form");
 
+const savedData = localStorage.getItem("feedback-form-state");
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email || "";
+  formData.message = parsedData.message || "";
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
 form.addEventListener("input", event => {
   formData[event.target.name] = event.target.value.trim();
 
   localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+});
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  if (formData.email === "" || formData.message === "") {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem("feedback-form-state");
+
+  formData.email = "";
+  formData.message = "";
+
+  form.reset();
 });
